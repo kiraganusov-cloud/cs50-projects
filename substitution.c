ec4508db6d1c03db81e54e9649e5f0d4de6495bd
kiraganusov-cloud <kiraganusov-cloud@users.noreply.github.com>
2025-12-29 18:35:28 -0800

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 string code_array;
+int check_key(string key);
 string convert(string plaintext);
 
 int main(int argc, string argv[])
@@ -21,10 +22,37 @@ int main(int argc, string argv[])
             return 1;
         }
     }
+    else if (check_key(argv[1]) != 0)
+    {
+        printf("Invalid key\n");
+    }
     code_array = argv[1];
 
     string plaintext = get_string("plaintext: ");
     printf("ciphertext: %s\n", convert(plaintext));
+    return 0;
+}
+
+int check_key(string key)
+{
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            return 1;
+        }
+    }
+    for (int i = 0; i < strlen(key); i++)
+    {
+        for (int j = i + 1; j < strlen(key); j++)
+        {
+            if (tolower(key[j]) == tolower(key[i]))
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
 string convert(string plaintext)
