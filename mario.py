e5af020c0ed9acbd79646c74da5a1e07aa50f494
kiraganusov-cloud <kiraganusov-cloud@users.noreply.github.com>
2026-02-01 14:23:25 -0800

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,6 +1,7 @@
-height = 0
-while (height < 1 or height > 8):
-    height = int(input("Height : "))
+height = int(input("Height : "))
 
-for i in range(1, height + 1):
-    print(" " * (height - i) + "*" * i)
+if (1 <= height <= 8):
+    for i in range(1, height + 1):
+        print(" " * (height - i) + "*" * i)
+else:
+    height = int(input("Height : "))
