018a6ccec1b1e8ff71d4524ea579226353496ecb
kiraganusov-cloud <kiraganusov-cloud@users.noreply.github.com>
2026-02-17 19:00:19 -0800

    automated commit by check50 [check50=True]

---
+++ 13.sql
@@ -0,0 +1,21 @@
+-- 13. Names of all people who starred in a movie in which Kevin Bacon also starred
+SELECT DISTINCT name
+FROM people
+WHERE id IN
+(
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN
+    (
+        SELECT movie_id
+        FROM stars
+        WHERE person_id =
+        (
+            SELECT id
+            FROM people
+            WHERE name = 'Kevin Bacon'
+        )
+    )
+)
+AND name != 'Kevin Bacon'
+ORDER BY name;
